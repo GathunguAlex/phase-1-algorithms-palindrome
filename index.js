7e7f80054f9438a8bf8e95f8584f2b898f4c013f
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-08-27 11:53:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
-*/
-
-/*
+*/this function takes a string as from user and processes it to check if its trully a palindrome and if not it returns false.
+ /*
   Add written explanation of your solution here
-*/
+*/ this function should checkin database if the user input is stored then if it matches,retuns true.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
